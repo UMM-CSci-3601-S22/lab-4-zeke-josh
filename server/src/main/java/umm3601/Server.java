1ddb5d158e4f6d662bc12109f5b81d08461e8da5
Zeke <krug0102@morris.umn.edu>
2022-02-24 15:20:33 -0600

    Add functionality to server todos and add/delete todos
    Co-authored-by: Shua59 <jbengelkes@gmail.com>

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -12,6 +12,7 @@ import io.javalin.Javalin;
 import io.javalin.core.util.RouteOverviewPlugin;
 import io.javalin.http.InternalServerErrorResponse;
 import umm3601.user.UserController;
+import umm3601.todo.TodoController;
 
 public class Server {
 
@@ -36,6 +37,7 @@ public class Server {
 
     // Initialize dependencies
     UserController userController = new UserController(database);
+    TodoController todoController = new TodoController(database);
 
     Javalin server = Javalin.create(config -> {
       config.registerPlugin(new RouteOverviewPlugin("/api"));
@@ -58,15 +60,28 @@ public class Server {
     // List users, filtered using query parameters
     server.get("/api/users", userController::getUsers);
 
+    // List todos, filtered using query parameters
+    server.get("/api/todos", todoController::getTodos);
+
     // Get the specified user
     server.get("/api/users/{id}", userController::getUser);
+
+    // Get the specified todo
+    server.get("/api/todos/{id}", todoController::getTodo);
 
     // Delete the specified user
     server.delete("/api/users/{id}", userController::deleteUser);
 
+    // Delete the specified todo
+    server.delete("/api/todos/{id}", todoController::deleteTodo);
+
     // Add new user with the user info being in the JSON body
     // of the HTTP request
     server.post("/api/users", userController::addNewUser);
+
+    // Add a new todo with the todo info being in the JSON body
+    // of the HTTP request
+    server.post("/api/todos", todoController::addNewTodo);
 
     // This catches any uncaught exceptions thrown in the server
     // code and turns them into a 500 response ("Internal Server
