be2a4a13677ee72e5630b0a59186637750896b37
Shua <enge0479@morris.umn.edu>
2022-02-28 13:18:24 -0600

    Alter add.todo.component.ts for fields of todos

--- client/src/app/todos/add-todo.component.ts
+++ client/src/app/todos/add-todo.component.ts
@@ -19,28 +19,27 @@ export class AddTodoComponent implements OnInit {
     // not sure if this name is magical and making it be found or if I'm missing something,
   // but this is where the red text that shows up (when there is invalid input) comes from
   addTodoValidationMessages = {
-    name: [
-      {type: 'required', message: 'Name is required'},
-      {type: 'minlength', message: 'Name must be at least 2 characters long'},
-      {type: 'maxlength', message: 'Name cannot be more than 50 characters long'},
-      {type: 'existingName', message: 'Name has already been taken'}
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
     ],
 
-    age: [
-      {type: 'required', message: 'Age is required'},
-      {type: 'min', message: 'Age must be at least 15'},
-      {type: 'max', message: 'Age may not be greater than 200'},
-      {type: 'pattern', message: 'Age must be a whole number'}
+    category: [
+      {type: 'required', message: 'Category is required'},
+      {type: 'minlength', message: 'Category must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Category cannot be more than 50 characters long'},
     ],
 
-    email: [
-      {type: 'email', message: 'Email must be formatted properly'},
-      {type: 'required', message: 'Email is required'}
+    body: [
+      {type: 'body', message: 'Body is required'},
+      {type: 'minlength', message: 'Body must be at least 5 characters long'},
+      {type: 'maxlength', message: 'Body cannot be more than 150 characters long'},
     ],
 
-    role: [
-      { type: 'required', message: 'Role is required' },
-      { type: 'pattern', message: 'Role must be Admin, Editor, or Viewer' },
+    status: [
+      { type: 'required', message: 'Status is required' },
+      { type: 'pattern', message: 'Status must be Complete or Incomplete' },
     ]
   };
 
@@ -52,7 +51,7 @@ export class AddTodoComponent implements OnInit {
     // add todo form validations
     this.addTodoForm = this.fb.group({
       // We allow alphanumeric input and limit the length for name.
-      name: new FormControl('', Validators.compose([
+      owner: new FormControl('', Validators.compose([
         Validators.required,
         Validators.minLength(2),
         // In the real world you'd want to be very careful about having
@@ -60,40 +59,23 @@ export class AddTodoComponent implements OnInit {
         // very long names. This demonstrates that it's possible, though,
         // to have maximum length limits.
         Validators.maxLength(50),
-        (fc) => {
-          if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
-            return ({existingName: true});
-          } else {
-            return null;
-          }
-        },
       ])),
 
-      // Since this is for a company, we need workers to be old enough to work, and probably not older than 200.
-      age: new FormControl('', Validators.compose([
+      category: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.min(15),
-        Validators.max(200),
-        // In the HTML, we set type="number" on this field. That guarantees that the value of this field is numeric,
-        // but not that it's a whole number. (The todo could still type -27.3232, for example.) So, we also need
-        // to include this pattern.
-        Validators.pattern('^[0-9]+$')
+        Validators.minLength(2),
+        Validators.maxLength(50),
       ])),
 
-      // We don't care much about what is in the company field, so we just add it here as part of the form
-      // without any particular validation.
-      company: new FormControl(),
-
-      // We don't need a special validator just for our app here, but there is a default one for email.
-      // We will require the email, though.
-      email: new FormControl('', Validators.compose([
+      body: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.email,
+        Validators.minLength(5),
+        Validators.maxLength(150),
       ])),
 
-      role: new FormControl('viewer', Validators.compose([
+      status: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.pattern('^(admin|editor|viewer)$'),
+        Validators.pattern('complete' || 'incomplete'),
       ])),
     });
 
@@ -106,7 +88,7 @@ export class AddTodoComponent implements OnInit {
 
   submitForm() {
     this.todoService.addTodo(this.addTodoForm.value).subscribe(newID => {
-      this.snackBar.open('Added todo ' + this.addTodoForm.value.name, null, {
+      this.snackBar.open('Added todo ' + this.addTodoForm.value.owner, null, {
         duration: 2000,
       });
       this.router.navigate(['/todos/', newID]);
