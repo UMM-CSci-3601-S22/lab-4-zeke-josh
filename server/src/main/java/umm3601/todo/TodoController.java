8201b659954db55c196faa68c2e45e54def7ae2c
Zeke Krug <krug0102@morris.umn.edu>
2022-03-02 15:55:47 -0600

    Try to fix `SimplifyBooleanExpression` checkstyle error

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -96,7 +96,7 @@ public class TodoController {
       // Verify that the Todo has a owner that is not blank
       .check(tdo -> tdo.owner != null && tdo.owner.length() > 0, "Todo must have a non-empty Todo owner")
       // Verify that the status is one of the valid status
-      .check(tdo -> tdo.status == true || false, "Todo must have a legal Todo status")
+      .check(tdo -> tdo.status || !tdo.status, "Todo must have a legal Todo status")
       // Verify that the Todo has a body that is not blank
       .check(tdo -> tdo.body != null && tdo.body.length() > 0, "Todo must have a legal body")
       // Verify that the Todo has a category that is not blank
